98d26967425b86c84fe4fdd7114baa53f3916bdf
ohhyunjong <141655749+jong001029@users.noreply.github.com>
2025-05-29 17:31:43 +0900

    202505291731 api테스트
    추가적으로 controller, domain, dto, repository, service 일정부분 추가했습니다.
    
    괜찮다면 이 파일 기반으로 만들어나가도 될 것 같습니다.

---
+++ BookManageMent/src/main/java/com/bookmanage/BookManageMent/domain/Book.java
@@ -0,0 +1,39 @@
+package com.bookmanage.BookManageMent.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.web.ErrorResponse;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "book")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer book_id;
+
+    @Column(nullable = false)
+    private Integer user_id;
+
+    @Column(nullable = false, length = 50)
+    private String book_name;
+
+    @Column(nullable = false)
+    private LocalDateTime create_date;
+
+    private LocalDateTime modify_date;
+
+    @Column(nullable = false, length = 500)
+    private String summary;
+
+    private String book_image;
+
+}
---
+++ BookManageMent/src/main/java/com/bookmanage/BookManageMent/dto/BookDTO.java
@@ -0,0 +1,16 @@
+package com.bookmanage.BookManageMent.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookDTO {
+    private String book_name;
+    private String summary;
+}
+
