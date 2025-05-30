863913f90bd86f4e21e173494bef8ba3fcd7125c
hyunjong2 <jong001029@naver.com>
2025-05-30 15:22:37 +0900

    jwtutil, logincontroller 추가

---
+++ BookManageMent/src/main/java/com/bookmanage/BookManageMent/util/JwtUtil.java
@@ -0,0 +1,33 @@
+package com.bookmanage.BookManageMent.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private final long expiration = 1000 * 60 * 60; // 1시간
+
+    public String generateToken(String userId) {
+        return Jwts.builder()
+                .setSubject(userId)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(key)
+                .compact();
+    }
+
+    public String validateAndGetUserId(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
