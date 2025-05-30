3dc24a1b410f1cd2bd1f8a81ca0b1c61ea97593e
jihyeon <jihyeon@gmail.com>
2025-05-30 10:40:59 +0900

    add BookDTO

--- BookManageMent/src/main/java/com/bookmanage/BookManageMent/dto/BookDTO.java
+++ BookManageMent/src/main/java/com/bookmanage/BookManageMent/dto/BookDTO.java
@@ -5,12 +5,50 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class BookDTO {
-    private String book_name;
-    private String summary;
+
+    @Setter @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Post{
+        private Integer user_id;    // 작성자
+        private String book_name;   // 제목
+        private String summary;     // 요약
+        private String book_image;    // 이미지
+    }
+
+    @Setter @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Put{
+        private String book_name;
+        private String summary;
+        private String book_image;
+    }
+
+
+    @Setter @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Response{
+        private Integer user_id;
+        private Integer book_id;
+        private String book_name;
+        private LocalDateTime create_date;
+        private LocalDateTime modify_date;
+        private String summary;
+        private String book_image;
+    }
+
+
+
+
+
 }
 
